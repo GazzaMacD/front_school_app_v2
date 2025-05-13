dfd3f9942d2fab217ed4c2e7c0a3d06b159d51e0
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-05-13 10:52:40 +0200

    Take out welcome component and to basic homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "英会話・語学学校 エクスリンガル" },
+    {
+      name: "description",
+      content:
+        "エクスリンガルの講師は全員、マルチリンガルなネイティブ講師です。当校の講師たちは長年の語学学習と専門的な語学教育を経験し、長久手市、みよし市、名古屋市東部を中心に、多くの方へ質の高い語学教育を提供しています。当校の語学学習サービスは、刺激的かつ非常に効果的です。複数の言語を習得したマルチリンガルな講師だからこそお伝えできる、効果的な学習法を取り入れています。日本での教育経験も豊富で、初級から上級までどのような語学レベルの生徒様であっても、それぞれのニーズに合わせた丁寧な指導を行います。ビジネス英語コースでは、一般ビジネス英語から、プレゼンテーションや交渉、会議などに特化したものまで幅広く指導します。また英検やTOEIC、その他試験対策も、当校の質の高いレッスンを求める生徒様が多くいらっしゃいます。もちろん一般英会話やフランス語コースも、数多くの生徒様に大変ご好評いただいております。無料でレベルチェックと体験レッスンを50分ずつ行っており、語学イベント「ラーニング・エクスペリエンス」も随時開催しています。語学に興味のある方はぜひ一度ご参加くださいね。素晴らしい時間を過ごせることでしょう。",
+    },
   ];
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: "Hello from Vercel" };
+  return { message: "Hello from XLingual" };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  const { message } = loaderData;
+  return <div>{message}</div>;
 }
