302e1e29301c64881fa5a706a40bf99c274d6bf9
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-09-24 11:49:47 +0200

    Courses parent page created

--- app/routes/courses.tsx
+++ app/routes/courses.tsx
@@ -1,5 +1,26 @@
+import type { Route } from "./+types/courses";
 import { Outlet } from "react-router";
 
+import cardStyles from "~/styles/components/cards.css?url";
+import coursesStyles from "~/styles/courses.css?url";
+
+/**
+ * Helpers
+ */
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: coursesStyles,
+  },
+  {
+    rel: "stylesheet",
+    href: cardStyles,
+  },
+];
+
+/**
+ * Page
+ */
 export default function Courses() {
   return <Outlet />;
 }
