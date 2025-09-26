74c0d760849c51f6b3477a10f5287462e6897995
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-09-26 05:13:52 +0200

    Staff parent page

--- app/routes/staff.tsx
+++ app/routes/staff.tsx
@@ -1,5 +1,21 @@
 import { Outlet } from "react-router";
 
+import type { Route } from "./+types/staff";
+import staffStyles from "~/styles/staff.css?url";
+
+/**
+ * Helpers
+ */
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: staffStyles,
+  },
+];
+
+/**
+ * Page
+ */
 export default function Staff() {
   return <Outlet />;
 }
