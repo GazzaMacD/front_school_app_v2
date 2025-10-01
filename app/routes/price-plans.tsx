d28a19ee3812b7d9608afcfc4ad9b87772a25c79
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-10-01 05:46:11 +0200

    Price plan parent page

--- app/routes/price-plans.tsx
+++ app/routes/price-plans.tsx
@@ -1,5 +1,20 @@
 import { Outlet } from "react-router";
+import type { Route } from "./+types/price-plans";
+import pricePlanStyles from "~/styles/price-plans.css?url";
 
+/**
+ * Helpers
+ */
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: pricePlanStyles,
+  },
+];
+
+/**
+ * Page
+ */
 export default function PricePlans() {
   return <Outlet />;
 }
