8a1160d62a6c0c120c69ce75058e8a1577900136
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-10-02 07:22:43 +0200

    Price plan detail page loader and types

--- app/routes/price-plans-detail.tsx
+++ app/routes/price-plans-detail.tsx
@@ -1,10 +1,90 @@
 import type { Route } from "./+types/price-plans-detail";
+import { BASE_API_URL, BASE_BACK_URL } from "~/.server/env";
+import { getTitle, getDesc, fetchWithMeta } from "~/common/utils";
+import { Swoosh1 } from "~/components/swooshes";
+import type { TDetailMeta, TFullImage, TRichTextBlock } from "~/common/types";
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: "Price Plans Detail route" };
+/**
+ * Loaders and Actions
+ */
+export async function loader({ params }: Route.LoaderArgs) {
+  const { slug } = params;
+  const priceDetailUrl = `${BASE_API_URL}/pages/?slug=${slug}&type=products.ClassPricesDetailPage&fields=*`;
+  const priceDetailPageResult = await fetchWithMeta<TPriceDetailPageResult>(
+    priceDetailUrl
+  );
+
+  //error
+  if (!priceDetailPageResult.success) {
+    console.error("Price Detail page failed:", priceDetailPageResult.error);
+    throw new Response("Sorry that is an error", {
+      status: priceDetailPageResult.status,
+    });
+  }
+
+  //success
+  return {
+    page: priceDetailPageResult.data.items[0],
+    base_back_url: BASE_BACK_URL,
+  };
 }
 
+/**
+ * Page
+ */
 export default function PricePlansDetail({ loaderData }: Route.ComponentProps) {
-  const { message } = loaderData;
-  return <div>{message}</div>;
+  const { page, base_back_url } = loaderData;
+  return <div>page here</div>;
 }
+
+/**
+ * Types
+ */
+type TClassPriceInfo = {
+  name: string;
+  display_name: string;
+  pretax_price: string;
+  posttax_price: string;
+  is_sale: boolean;
+  start_date: string; // ISO timestamp
+  is_limited_sale: boolean;
+  before_sale_pretax_price: string | null;
+  before_sale_posttax_price: string | null;
+  end_date: string | null;
+};
+
+type TClassService = {
+  id: number;
+  name: string;
+  slug: string;
+  class_type: string;
+  class_type_display: string;
+  min_num: number;
+  max_num: number;
+  length: number;
+  length_unit: string;
+  quantity: number;
+  quantity_unit: string;
+  is_native: boolean;
+  is_online: boolean;
+  is_inperson: boolean;
+  has_onlinenotes: boolean;
+  bookable_online: boolean;
+  price_info: TClassPriceInfo;
+};
+
+type TPriceDetailPage = {
+  id: number;
+  meta: TDetailMeta;
+  title: string;
+  class_service: TClassService;
+  display_title: string;
+  display_tagline: string;
+  header_image: TFullImage;
+  class_intro: TRichTextBlock[];
+};
+
+type TPriceDetailPageResult = {
+  meta: { total_count: number };
+  items: TPriceDetailPage[];
+};
