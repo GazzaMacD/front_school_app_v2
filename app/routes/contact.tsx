380d15b247ed342e3a350e0cc8c99536e024fc64
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-10-14 10:45:31 +0200

    Contact page parent route

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,5 +1,32 @@
 import { Outlet } from "react-router";
 
+import type { Route } from "./+types/contact";
+
+import contactStyles from "~/styles/contact.css?url";
+import pageStyles from "~/styles/components/pages.css?url";
+import cardStyles from "~/styles/components/cards.css?url";
+
+/**
+ * Helpers
+ */
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: contactStyles,
+  },
+  {
+    rel: "stylesheet",
+    href: pageStyles,
+  },
+  {
+    rel: "stylesheet",
+    href: cardStyles,
+  },
+];
+
+/**
+ * Page
+ */
 export default function Contact() {
   return <Outlet />;
 }
