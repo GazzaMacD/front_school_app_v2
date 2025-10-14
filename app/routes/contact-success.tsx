68dc0671a891719a9ae151d76cda1834163025ea
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-10-14 10:53:07 +0200

    Contact success page

--- app/routes/contact-success.tsx
+++ app/routes/contact-success.tsx
@@ -1,10 +1,45 @@
-import type { Route } from "./+types/contact-success";
+import { Link, redirect } from "react-router";
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: "Contact Success route" };
+import type { Route } from "./+types/contact-success";
+import { HeadingOne } from "~/components/headings";
+import { Swoosh1 } from "~/components/swooshes";
+
+export function loader({ request }: Route.LoaderArgs) {
+  const referer = request.headers.get("referer");
+  const pathname = referer ? new URL(referer).pathname : "";
+  if (pathname === "/contact") {
+    return null;
+  }
+  return redirect("/");
 }
 
-export default function ContactSuccess({ loaderData }: Route.ComponentProps) {
-  const { message } = loaderData;
-  return <div>{message}</div>;
+export default function ContactSuccess() {
+  return (
+    <>
+      <div className="ct-success">
+        <div className="g-narrow-container">
+          <div className="ct-success__heading">
+            <HeadingOne
+              enText="Success"
+              jpText="送信しました"
+              align="center"
+              bkground="light"
+              level="h1"
+            />
+          </div>
+          <div className="ct-success__details">
+            <p>
+              お問い合わせいただき誠にありがとうございます。こちらから改めてご連絡させていただきますのでお待ち下さいませ。
+            </p>
+            <p>
+              もしよろしければ、
+              <Link to="/blog-lessons">ブログレッスン一覧</Link>や、
+              <Link to="/">エクスリンガルホームページ</Link>をご覧ください。
+            </p>
+          </div>
+        </div>
+      </div>
+      <Swoosh1 backColor="cream" swooshColor="beige" />
+    </>
+  );
 }
