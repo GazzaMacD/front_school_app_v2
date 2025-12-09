adff1aa64633e9ea5cee20ca960a49d41f151767
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-12-09 15:26:30 +0100

    Dashboard header complete

--- app/routes/my-page.tsx
+++ app/routes/my-page.tsx
@@ -1,11 +1,51 @@
-import { Outlet } from "react-router";
+import * as React from "react";
 
+import { Outlet, Link } from "react-router";
+
+import myPageGlobalStyles from "~/styles/mypage-global.css?url";
+// type imports
+import type { Route } from "./+types/my-page";
+
+/**
+ * Helpers
+ */
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: myPageGlobalStyles,
+  },
+];
+
+/**
+ * Page
+ */
 export default function MyPage() {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
   return (
     <>
-      <header>Header for My Page</header>
-      <aside>Aside for my page</aside>
-      <Outlet />
+      <header className="mpg-header">
+        <Link to="/" className="mpg-header__logo">
+          <div>
+            <img
+              src="/img/x_only_xlingual_logo.svg"
+              alt="xlingual x only logo"
+            />
+          </div>
+        </Link>
+        <button
+          className={`mpg-header__hamburger ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <div>
+            <span></span>
+          </div>
+        </button>
+      </header>
+      <aside></aside>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
