643cbf5bc357f985b48412f6bd61b18ab051c033
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-12-10 11:16:00 +0100

    Dashboard sliding menu completed, possible small adjustments later + need to remove hard coded names when auth implemented

--- app/routes/my-page.tsx
+++ app/routes/my-page.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 
-import { Outlet, Link } from "react-router";
+import { Outlet, Link, NavLink } from "react-router";
+import { RxDashboard } from "react-icons/rx";
+import {
+  BsPersonCircle,
+  BsCalendarWeek,
+  BsPersonVcard,
+  BsPersonVideo2,
+  BsHouse,
+} from "react-icons/bs";
 
 import myPageGlobalStyles from "~/styles/mypage-global.css?url";
 // type imports
@@ -42,7 +50,86 @@ export default function MyPage() {
           </div>
         </button>
       </header>
-      <aside></aside>
+      <aside className={`mpg-menu ${menuOpen ? "open" : ""}`}>
+        <div className="mpg-menu__top">
+          <BsPersonCircle />
+          <span>Gareth Macdonald</span>
+        </div>
+        <div className="mpg-menu__middle">
+          <nav className="mpt-menu__middle_nav">
+            <ul>
+              <li>
+                <NavLink
+                  to="/my-page"
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                  end
+                >
+                  <RxDashboard />
+                  マイページホーム
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/my-page/schedules"
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                  end
+                >
+                  <BsCalendarWeek />
+                  スケジュール
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/my-page/video-calls"
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                  end
+                >
+                  <BsPersonVideo2 />
+                  ビデオ通話
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/my-page/profile"
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                  end
+                >
+                  <BsPersonVcard />
+                  プロフィール
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                  end
+                >
+                  <BsHouse />
+                  ホームページ
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="mpg-menu__bottom">
+          <p>macdonald.gareth@gmail.com</p>
+          <form action="/logout" method="post">
+            <button className="mpg-menu__logout" type="submit">
+              ログアウト
+            </button>
+          </form>
+        </div>
+      </aside>
       <main>
         <Outlet />
       </main>
