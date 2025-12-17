eca067bcb105db17f06b61386fff3db61aec4f67
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-12-17 16:58:19 +0100

    Video Calls parent route

--- app/routes/myp-video-calls.tsx
+++ app/routes/myp-video-calls.tsx
@@ -1,9 +1,69 @@
-import { Outlet } from "react-router";
+import { Outlet, redirect, data } from "react-router";
 
-export default function VideoCalls() {
+import {
+  authenticatedUser,
+  createAuthenticatedHeaders,
+} from "~/.server/session";
+import { fetchWithMeta } from "~/common/utils";
+import { BASE_API_URL } from "~/.server/env";
+//type imports
+import type { Route } from "./+types/myp-video-calls";
+
+/*
+ * Loaders and Actions
+ */
+export async function loader({ request }: Route.LoaderArgs) {
+  const userData = await authenticatedUser(request);
+  const redirectTo = new URL(request.url).pathname;
+  const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+  //if null get path and redirect to login route with redirect parameter
+  if (!userData) {
+    return redirect(`/login?${searchParams}`);
+  }
+  try {
+    const headers = await createAuthenticatedHeaders(userData.access);
+    const options = {
+      method: "GET",
+      headers,
+    };
+    const videoCallsUrl = `${BASE_API_URL}/video-calls/`;
+    const videoCallsData = await fetchWithMeta<TVideoCalls>({
+      url: videoCallsUrl,
+      options,
+    });
+
+    if (!videoCallsData.success) {
+      return redirect(`/login?${searchParams}`);
+    }
+    return data({
+      videoCalls: videoCallsData.data,
+      user: userData.user,
+    });
+  } catch (error) {
+    console.error(`Error in loader at ${redirectTo}: ${error}`);
+    throw data("Ooops that is a 500", { status: 500 });
+  }
+}
+
+/*
+ * Page
+ */
+export default function VideoCalls({ loaderData }: Route.ComponentProps) {
+  const { user, videoCalls } = loaderData;
   return (
     <>
       <Outlet />
     </>
   );
 }
+
+/*
+ * Types
+ */
+type TVideoCall = {
+  slug: string;
+  teacher: { name: string; id: number };
+  host_room_url: string;
+  room_url: string;
+};
+type TVideoCalls = TVideoCall[];
