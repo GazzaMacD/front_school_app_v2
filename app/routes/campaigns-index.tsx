2194de8a568de85a12515137f93353307ff57f48
Gareth Macdonald <macdonald.gareth@gmail.com>
2026-01-27 19:09:44 +0100

    Campaign detail page implemented

--- app/routes/campaigns-index.tsx
+++ app/routes/campaigns-index.tsx
@@ -1,10 +1,5 @@
-import type { Route } from "./+types/campaigns-index";
+import { redirect } from "react-router";
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: "Campaigns Index Page" };
-}
-
-export default function CampaignIndex({ loaderData }: Route.ComponentProps) {
-  const { message } = loaderData;
-  return <div>{message}</div>;
+export function loader() {
+  return redirect("/");
 }
